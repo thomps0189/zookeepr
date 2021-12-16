76a203dc166e569138696e5965dc449b9622e7f8
thomps0189 <angelathompson2011@gmail.com>
2021-12-16 10:21:55 -0600

    front end linked

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -31,6 +31,18 @@ const getAnimals = (formData = {}) => {
 
   console.log(queryUrl);
 
+  fetch(queryUrl)
+  .then(response => {
+    if (!response.ok) {
+      return alert('Error: ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(animalData => {
+    console.log(animalData);
+    printResults(animalData);
+  });
+
 };
 
 const handleGetAnimalsSubmit = event => {
@@ -60,6 +72,8 @@ const handleGetAnimalsSubmit = event => {
   const animalObject = { diet, personalityTraits };
 
   getAnimals(animalObject);
+
+  
 };
 
 $animalForm.addEventListener('submit', handleGetAnimalsSubmit);
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -113,9 +114,27 @@ app.post("/api/animals", (req, res) => {
     }    
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
+
+
 
 
 
